863d83a938173ef83b591b656bc4389543dc0c60
Stefan Sokolovic <stefan.sokolovic2@amd.com>
2025-08-15 20:53:42 +0200

    [hipBLASLt] Minor change to MUBUFModifiers in rocisa

--- projects/hipblaslt/tensilelite/rocisa/rocisa/include/container.hpp
+++ projects/hipblaslt/tensilelite/rocisa/rocisa/include/container.hpp
@@ -234,10 +234,6 @@ namespace rocisa
             {
                 kStr += " offen offset:" + std::to_string(offset12);
             }
-            if(glc || slc || lds || dlc || scope != CacheScope::SCOPE_NONE)
-            {
-                kStr += ",";
-            }
             if(hasGLCModifier && glc)
             {
                 kStr += " " + getGlcBitName(hasGLCModifier);
